a09d006c9c36f98760558d9b34610e4d23ecb0e5
FrontendFlair <hamaadafzal404@gmail.com>
2024-10-08 03:17:41 -0700

    commit

--- app/page.js
+++ app/page.js
@@ -1,23 +1,22 @@
-import WhoWeAre from '@/components/AboutUs'
-import Hero from '@/components/Hero'
-import Navbar from '@/components/Navbar'
-import VoltageElectricitySlider from '@/components/Products'
-import Service from '@/components/Services'
-import Stats from '@/components/Stats'
-import React from 'react'
+import WhoWeAre from "@/components/AboutUs";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import VoltageElectricitySlider from "@/components/Products";
+import Service from "@/components/Services";
+import Stats from "@/components/Stats";
+import React from "react";
 
 const page = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <WhoWeAre/>
-      <VoltageElectricitySlider/>
-      <Service/>
-      <Stats/>
-      <div className="mb-[3000px]"></div>
+      <Navbar />
+      <Hero />
+      <WhoWeAre />
+      <VoltageElectricitySlider />
+      <Service />
+      <Stats />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
